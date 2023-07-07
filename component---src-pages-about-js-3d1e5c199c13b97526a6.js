c39b7eb62a36445e80ff1121a7580892716ceea8
mango-tree <contact@mingyu.kim>
2023-07-08 03:18:52 +0900

    Updates

---
+++ component---src-pages-about-js-3d1e5c199c13b97526a6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[682],{6970:function(t,e,l){l.r(e),l.d(e,{Head:function(){return c}});var a=l(959),n=l(7209),r=l(2);const c=()=>a.createElement(r.Z,{title:"About"});e.default=t=>{let{data:e,location:l}=t;const r=e.site.siteMetadata.title;return console.log(e),a.createElement(n.Z,{location:l,title:r},a.createElement("h2",null,"Mingyu Kim"),a.createElement("p",null,"개발하는 사람."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-3d1e5c199c13b97526a6.js.map
